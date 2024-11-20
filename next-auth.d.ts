4917f4635f92dde55f02848ca62f1369a1b5e632
Forrest Herman <forrest.herman@otolawn.com>
2024-11-20 00:48:52 -0500

    Fix time comparison for refresh tokens

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,8 +1,29 @@
 import NextAuth from "next-auth";
 
-declare module "next-auth" {
-    interface Session {
-        access_token?: string;
-        spreadsheet_id?: string;
-    }
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      email?: string;
+      name?: string;
+      image?: string;
+      sub?: string;
+    };
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpires: string;
+    spreadsheet_id?: string;
+  }
+
+  interface JWT {
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpires: number;
+    user: {
+      email?: string;
+      name?: string;
+      image?: string;
+      sub?: string;
+    };
+  }
 }
